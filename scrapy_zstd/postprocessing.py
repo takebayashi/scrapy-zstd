23ade59b9a796afcdae1b43c13c756c97b6f6dd3
Shun Takebayashi <shun@takebayashi.asia>
2024-03-23 23:16:37 +0900

    Add ZstdPlugin

---
+++ scrapy_zstd/postprocessing.py
@@ -0,0 +1,14 @@
+from typing import Any, BinaryIO, Dict
+
+from zstandard import ZstdCompressor
+
+
+class ZstdPlugin:
+    def __init__(self, file: BinaryIO, feed_options: Dict[str, Any]) -> None:
+        self.writer = ZstdCompressor().stream_writer(file)
+
+    def write(self, data: bytes) -> int:
+        return self.writer.write(data)
+
+    def close(self) -> None:
+        self.writer.close()
---
+++ tests/test_postprocessing.py
@@ -0,0 +1,18 @@
+from io import BytesIO
+
+from zstandard import MAGIC_NUMBER, decompress
+
+from scrapy_zstd.postprocessing import ZstdPlugin
+
+
+def test_compress_zstd() -> None:
+    output_file = BytesIO()
+    output_file.close = lambda: None  # type: ignore
+
+    plugin = ZstdPlugin(output_file, {})
+    plugin.write(b"hello")
+    plugin.close()
+
+    compressed_data = output_file.getvalue()
+    assert int.from_bytes(compressed_data[:4], "little") == MAGIC_NUMBER
+    assert decompress(compressed_data, 1024) == b"hello"
